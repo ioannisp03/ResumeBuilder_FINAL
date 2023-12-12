2bbb437693ecbf607bef82a90d78e4c35d9d7557
CharlesS42 <2130442@champlaincollege.qc.ca>
2023-12-12 15:00:38 -0500

    Update on my Contact DB

--- ResumeBuilder_FINAL/Contact.cs
+++ ResumeBuilder_FINAL/Contact.cs
@@ -8,6 +8,18 @@ namespace ResumeBuilder_FINAL
 {
     public class Contact
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
 
+        public override string ToString()
+        {
+            string display = String.Format("{0}\t {1}\t {2}\t {3}\t {4}\t {5}\t {6}", Id, FirstName, LastName, Age, PhoneNumber, Email, Position);
+            return display;
+        }
     }
 }
---
+++ ResumeBuilder_FINAL/ContactDBHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeBuilder_FINAL
+{
+    public sealed class ContactDBHandler
+    {
+        static readonly string ConString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+        private static readonly ContactDBHandler instance = new ContactDBHandler();
+
+        private ContactDBHandler()
+        {
+            CreateContactTable();
+        }
+
+        public static ContactDBHandler Instance
+        {
+            get { return instance; }
+        }
+
+        public void CreateContactTable()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(ConString))
+            {
+                con.Open();
+                SQLiteCommand dropCommand = new SQLiteCommand("drop table if exists CONTACTS;", con);
+                dropCommand.ExecuteNonQuery();
+
+                string contactTable = "create table CONTACTS (ID integer primary key, FirstName text, LastName text," +
+                    "Age integer, PhoneNumber text, Email text, Position text)";
+                SQLiteCommand createCommand = new SQLiteCommand(contactTable, con);
+                createCommand.ExecuteNonQuery();
+            }
+        }
+
+        public int AddContact(Contact contact) 
+        { 
+            int newId = 0;
+            int rows = 0;
+
+            using (SQLiteConnection con = new SQLiteConnection())
+            {
+                con.Open();
+
+                string insertQuery = "INSERT INTO CONTACTS (FirstName, LastName, Age, PhoneNumber, Email, Position) " +
+                "VALUES (@FirstName, @LastName, @Age, @PhoneNumber, @Email, @Position)";
+
+                SQLiteCommand insertCom = new SQLiteCommand(insertQuery, con);
+
+                insertCom.Parameters.AddWithValue("@FirstName", contact.FirstName);
+                insertCom.Parameters.AddWithValue("@LastName", contact.LastName);
+                insertCom.Parameters.AddWithValue("@Age", contact.Age);
+                insertCom.Parameters.AddWithValue("@PhoneNumber", contact.PhoneNumber);
+                insertCom.Parameters.AddWithValue("@Email", contact.Email);
+                insertCom.Parameters.AddWithValue("@Position", contact.Position);
+
+                try
+                {
+                    rows = insertCom.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex) 
+                { 
+                
+                }
+            }
+
+            return newId;
+        }
+
+    }
+}
