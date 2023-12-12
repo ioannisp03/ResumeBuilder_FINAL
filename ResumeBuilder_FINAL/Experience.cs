52571770b67adfec697971d6983990be54ff439b
Adem Bessam <2232468@champlaincollege.qc.ca>
2023-12-12 17:31:11 -0500

    Finished the ExperienceDBHandler

---
+++ ResumeBuilder_FINAL/Experience.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeBuilder_FINAL
+{
+    public class Experience
+    {
+        public int Id { get; set; }
+        public string CompagnyName { get; set; }
+        public string StartedDate { get; set; }
+        public string EndedDate { get; set; }
+        public string Position { get; set; }
+
+        public override string ToString()
+        {
+            string display = String.Format("{0}\t {1}\t {2}\t {3}\t {4}\t {5}\t {6}", Id, CompagnyName, StartedDate, EndedDate, Position);
+            return display;
+        }
+    }
+}
