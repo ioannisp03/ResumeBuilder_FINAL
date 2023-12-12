b7b6884c0a6aa601abac3039674c39d77456da55
ioannis panaritis <i.panar@yahoo.com>
2023-12-12 14:16:13 -0500

    created windows for addEducationalExperiences and editEducationalExperiences

---
+++ ResumeBuilder_FINAL/AddEducationWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResumeBuilder_FINAL
+{
+    /// <summary>
+    /// Interaction logic for AddEducationWindow.xaml
+    /// </summary>
+    public partial class AddEducationWindow : Window
+    {
+        public AddEducationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
