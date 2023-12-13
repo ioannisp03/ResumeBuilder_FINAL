09789dcc6b7de03817b4c7513422bd494df745db
CharlesS42 <2130442@champlaincollege.qc.ca>
2023-12-12 21:35:54 -0500

    Succesful Design for PDF

--- ResumeBuilder_FINAL/ExportToPDF.cs
+++ ResumeBuilder_FINAL/ExportToPDF.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace ResumeBuilder_FINAL
 {
+    /*
     public static class ExportToPDF
     {
         public static void exportToPDF(List<Contact> contactInfo, List<Experience> experiences, List<Education> diplomas)
@@ -37,19 +38,19 @@ namespace ResumeBuilder_FINAL
             XRect rect = new XRect(0, 10, page.Width - 20, 50);
             tf.Alignment = XParagraphAlignment.Center;
             string title = currentContact.FirstName + " " + currentContact.LastName;
-            tf.DrawString(title, fontTitle, XBrushes.BlueViolet, rect, XStringFormats.Center);
+            tf.DrawString(title, fontTitle, XBrushes.BlueViolet, rect);
 
             string contactText = String.Format("{0}\n{1} - {2} - {3} years old", currentContact.Position, currentContact.PhoneNumber, currentContact.Email, currentContact.Age);
             rect = new XRect(0, 5, page.Width - 20, 30);
             tf.Alignment = XParagraphAlignment.Center;
-            tf.DrawString(contactText, fontRegular, XBrushes.Black, rect, XStringFormats.Center);
+            tf.DrawString(contactText, fontRegular, XBrushes.Black, rect);
 
             rect = new XRect(0, 5, page.Width - 20, 30);
             tf.Alignment = XParagraphAlignment.Center;
 
 
             string subtitleExperience = "Professional Experience";
-            tf.DrawString(subtitleExperience, fontCategories, XBrushes.BlueViolet, rect, XStringFormats.Center);
+            tf.DrawString(subtitleExperience, fontCategories, XBrushes.BlueViolet, rect);
 
             string experiencesText = "";
             foreach(Experience exp in experiences)
@@ -61,7 +62,7 @@ namespace ResumeBuilder_FINAL
             tf.DrawString(experiencesText, fontRegular, XBrushes.Black, rect, XStringFormats.TopLeft);
 
             string subtitleEducation = "Education";
-            tf.DrawString(subtitleEducation, fontCategories, XBrushes.BlueViolet, rect, XStringFormats.Center);
+            tf.DrawString(subtitleEducation, fontCategories, XBrushes.BlueViolet, rect);
 
             string educationText = "";
             foreach(Education education in diplomas)
@@ -83,4 +84,86 @@ namespace ResumeBuilder_FINAL
             }
         }
     }
+    */
+    public static class ExportToPDF
+    {
+        public static void exportToPDF(List<Contact> contactInfo, List<Experience> experiences, List<Education> diplomas)
+        {
+            Contact currentContact = contactInfo[0];
+
+            PdfDocument pdfDocument = new PdfDocument();
+            pdfDocument.Info.Title = $"{currentContact.FirstName} {currentContact.LastName} Resume";
+
+            PdfPage page = pdfDocument.AddPage();
+            page.Size = PdfSharp.PageSize.Letter;
+
+            // -- styling --
+
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            XFont fontTitle = new XFont("Arial", 30, XFontStyleEx.Bold);
+            XFont fontCategories = new XFont("Times New Roman", 20, XFontStyleEx.Bold);
+            XFont fontRegular = new XFont("Times New Roman", 11, XFontStyleEx.Regular);
+
+            XTextFormatter tf = new XTextFormatter(gfx);
+
+            // Title
+            XRect rect = new XRect(0, 10, page.Width - 20, 50);
+            tf.Alignment = XParagraphAlignment.Center;
+            string title = $"{currentContact.FirstName} {currentContact.LastName}";
+            tf.DrawString(title, fontTitle, XBrushes.BlueViolet, rect);
+
+            // Contact Information
+            string contactText = $"{currentContact.Position}\n{currentContact.PhoneNumber} | {currentContact.Email} | {currentContact.Age} years old";
+            rect = new XRect(0, 70, page.Width - 20, 30);
+            tf.Alignment = XParagraphAlignment.Center;
+            tf.DrawString(contactText, fontRegular, XBrushes.Black, rect);
+
+            // Professional Experience
+            rect = new XRect(0, 110, page.Width - 20, 30);
+            tf.Alignment = XParagraphAlignment.Center;
+            string subtitleExperience = "Professional Experience";
+            tf.DrawString(subtitleExperience, fontCategories, XBrushes.BlueViolet, rect);
+
+            string experiencesText = "";
+            foreach (Experience exp in experiences)
+            {
+                experiencesText += $"\n\t{exp.Position} ({exp.StartedDate}-{exp.EndedDate}) at {exp.CompagnyName}";
+            }
+            rect = new XRect(0, 140, page.Width - 20, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString(experiencesText, fontRegular, XBrushes.Black, rect, XStringFormats.TopLeft);
+
+            // Education
+            rect = new XRect(0, 380, page.Width - 20, 30);
+            tf.Alignment = XParagraphAlignment.Center;
+            string subtitleEducation = "Education";
+            tf.DrawString(subtitleEducation, fontCategories, XBrushes.BlueViolet, rect);
+
+            string educationText = "";
+            foreach (Education education in diplomas)
+            {
+                educationText += $"\n{education.AcademicDegree}: {education.Major_FieldOfStudy} - {education.InstitutionName}, {education.YearOfCompletion}\n\t{education.Details}";
+            }
+
+            rect = new XRect(0, 410, page.Width - 20, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString(educationText, fontRegular, XBrushes.Black, rect, XStringFormats.TopLeft);
+
+            // Save PDF
+            const string filename = "Resume.pdf";
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files(*.pdf)|*.pdf|All files(*.*)|*.*";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog.Title = filename;
+            saveFileDialog.OverwritePrompt = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                pdfDocument.Save(saveFileDialog.FileName);
+            }
+        }
+    }
+
 }
