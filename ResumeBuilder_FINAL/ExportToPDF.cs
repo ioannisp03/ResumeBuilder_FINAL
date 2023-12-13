81768fd62502ceebaaa74b7bf499065195762737
CharlesS42 <2130442@champlaincollege.qc.ca>
2023-12-12 21:08:36 -0500

    ExportToPDF supposed to work

--- ResumeBuilder_FINAL/ExportToPDF.cs
+++ ResumeBuilder_FINAL/ExportToPDF.cs
@@ -5,6 +5,7 @@ using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,25 +37,50 @@ namespace ResumeBuilder_FINAL
             XRect rect = new XRect(0, 10, page.Width - 20, 50);
             tf.Alignment = XParagraphAlignment.Center;
             string title = currentContact.FirstName + " " + currentContact.LastName;
-            tf.DrawString(title, fontTitle, XBrushes.BlueViolet, rect);
+            tf.DrawString(title, fontTitle, XBrushes.BlueViolet, rect, XStringFormats.Center);
 
             string contactText = String.Format("{0}\n{1} - {2} - {3} years old", currentContact.Position, currentContact.PhoneNumber, currentContact.Email, currentContact.Age);
             rect = new XRect(0, 5, page.Width - 20, 30);
             tf.Alignment = XParagraphAlignment.Center;
-            tf.DrawString(contactText, fontRegular, XBrushes.Black, rect);
+            tf.DrawString(contactText, fontRegular, XBrushes.Black, rect, XStringFormats.Center);
 
             rect = new XRect(0, 5, page.Width - 20, 30);
             tf.Alignment = XParagraphAlignment.Center;
 
 
             string subtitleExperience = "Professional Experience";
-            tf.DrawString(subtitleExperience, fontCategories, XBrushes.BlueViolet, rect);
+            tf.DrawString(subtitleExperience, fontCategories, XBrushes.BlueViolet, rect, XStringFormats.Center);
 
-            
+            string experiencesText = "";
+            foreach(Experience exp in experiences)
+            {
+                experiencesText += String.Format("\n\t{0} ({1}-{2}) at {3}", exp.Position, exp.StartedDate, exp.EndedDate, exp.CompagnyName);
+            }
+            rect = new XRect(0, 220, page.Width - 20, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString(experiencesText, fontRegular, XBrushes.Black, rect, XStringFormats.TopLeft);
 
             string subtitleEducation = "Education";
-            tf.DrawString(subtitleEducation, fontCategories, XBrushes.BlueViolet, rect);
+            tf.DrawString(subtitleEducation, fontCategories, XBrushes.BlueViolet, rect, XStringFormats.Center);
 
+            string educationText = "";
+            foreach(Education education in diplomas)
+            {
+                educationText += String.Format("{0}: {1} - {2}, {3}\n\t{4}", education.AcademicDegree, education.Major_FieldOfStudy, education.InstitutionName, education.YearOfCompletion, education.Details);
+            }
+
+            const string filename = "Resume.pdf";
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files(*.pdf)|*.pdf|All files(*.*)|*.*";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog.Title = filename;
+            saveFileDialog.OverwritePrompt = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                pdfDocument.Save(saveFileDialog.FileName);
+            }
         }
     }
 }
